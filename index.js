396c6eac94434d7d3b8cb663c4f0b68bf789074b
AhmadTubaileh <tubailehahmad82@gmail.com>
2026-07-11 20:14:27 +0300

    full CRUD api

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
 require("dotenv").config();
 
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req,res) =>{
-    res.end("Hello world!");
+app.use(express.json());
+
+const users = [
+    {
+        "id":1,
+        "name":"Ahmad"
+    },
+    {
+        "id":2,
+        "name":"Sami"
+    }
+];
+
+app.get("/",(req,res)=>{
+    res.send("Home");
+});
+
+app.get("/about",(req,res)=>{
+    res.end("about");
+});
+
+app.get("/users",(req,res)=>{
+    res.send(users);
+});
+
+app.get("/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find(u=>u.id === id);
+
+    if(!user){
+        res.status(404).json({
+            "msg":"user not found"
+        });
+    }
+    res.json(user);
 
 });
 
-server.listen(process.env.PORT,()=>{console.log(process.env);});
+
+app.post("/users",(req,res)=>{
+    const newUser={
+        id:users.length+1,
+        name:req.body.name
+    };
+
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT);
