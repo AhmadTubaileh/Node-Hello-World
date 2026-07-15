6e5ea2c2a1e94393cccc36be5b4c1849aa247ede
AhmadTubaileh <tubailehahmad82@gmail.com>
2026-07-15 05:12:36 +0300

    JWT was added + fixed as clean code

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@
 const express = require("express");
 const app = express();
 const todoRoutes = require("./routes/todo.routes");
+const registerRoutes = require("./routes/register.routes");
 const loginRoutes = require("./routes/login.routes");
+const authMiddleware = require("./middleware/auth.middleware");
 
 app.use(express.json());
+app.use("/register",registerRoutes);
 app.use("/login",loginRoutes);
-app.use("/todo",todoRoutes);
+app.use("/todo", authMiddleware, todoRoutes);
 
 module.exports = app;
